342d2e9d458f15b01f8b6e328866b59d65c02fbf
Sergio <asgarth@gmail.com>
2020-10-29 18:20:57 +0100

    Link peakd benchmark tool

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -19,6 +19,20 @@
     <div class="mt-12 pl-8 pr-8">
       <node-table></node-table>
     </div>
+    <div class="container relative mx-auto mt-12">
+      <div class="items-center flex flex-wrap">
+        <div class="w-full ml-auto mr-auto text-center">
+          <div class="justify-center text-center">
+            <h2 class="text-white font-semibold text-3xl typing">
+              Test your nodes speed on peakd.com
+            </h2>
+            <button class="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-base my-3 px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+              <a href="https://peakd.com/me/tools/benchmark">Try it now</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
     <footer-component />
   </div>
 </template>
