b185130dff60ac86398cc22b738e5b098f39f179
Sergio Sartorelli <asgarth@gmail.com>
2021-07-02 23:43:31 +0200

    Improve API response for '/best' nodes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
 import { ScannerService, NodeStatus } from './scanner/scanner.service';
 
 export type NodeScore = {
@@ -16,12 +22,14 @@ const fromNodeStatus = (node: NodeStatus): NodeScore => ({
   score: node.score,
   updated_at: node.updated_at,
   success: node.tests.filter(t => t.success).length,
-  fail: node.tests.filter(t => !t.success).length
-})
+  fail: node.tests.filter(t => !t.success).length,
+});
 
+const BEST_NODES_SCORE_THRESHOLD = 100;
+const VALID_NODES_SCORE_THRESHOLD = 75;
+const MIN_NODES = 5;
 @Controller()
 export class AppController {
-
   constructor(private readonly scannerService: ScannerService) {}
 
   @Get('ping')
@@ -31,9 +39,24 @@ export class AppController {
 
   @Get('best')
   getBest(): NodeScore[] {
-    return this.scannerService.getNodes()
-      .filter(n => !n.website_only)
-      .filter(n => n.score > 75)
+    const nodes = this.scannerService
+      .getNodes()
+      .filter(n => n.score > 0 && !n.website_only);
+
+    const bestNodes: NodeStatus[] = nodes.filter(
+      n => n.score >= BEST_NODES_SCORE_THRESHOLD,
+    );
+
+    // if there are at least MIN_NODES with the best score -> return
+    if (bestNodes.length >= MIN_NODES) {
+      return bestNodes
+        .map(n => fromNodeStatus(n))
+        .sort((a, b) => b.score - a.score);
+    }
+
+    // otherwise reduce the filter threshold
+    return nodes
+      .filter(n => n.score >= VALID_NODES_SCORE_THRESHOLD)
       .map(n => fromNodeStatus(n))
       .sort((a, b) => b.score - a.score);
   }
@@ -52,5 +75,4 @@ export class AppController {
 
     return node;
   }
-
 }
